ce76653cbbdaceb9d0acc1eae06aa9da60409cd4
imad <imadelfahssi@gmail.com>
2020-12-08 12:11:06 +0100

    Creacion de dos objetos

--- src/main/java/practica4/Clase_Prueba_ImadELFahssi.java
+++ src/main/java/practica4/Clase_Prueba_ImadELFahssi.java
@@ -10,5 +10,11 @@ package practica4;
  * @author MSI
  */
 public class Clase_Prueba_ImadELFahssi {
-    
+    public static void main(String[] args) {
+       Clase persona1 = new Clase(176, "tirantes", true, 67.4);
+       Clase persona2 = new Clase(156, "manga corta", false, 127.4);
+       
+        System.out.println(persona1);
+        System.out.println(persona2);
+    }
 }
